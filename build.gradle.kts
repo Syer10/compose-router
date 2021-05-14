0bf300108c5920a0c20726afb937036d1a54c6f5
Syer10 <syer10@users.noreply.github.com>
2021-05-14 14:42:09 -0400

    Release to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+import com.vanniktech.maven.publish.MavenPublishPluginExtension
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.compose.compose
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.Properties
 
 plugins {
     kotlin("jvm") version "1.4.32"
     id("org.jetbrains.compose") version "0.4.0-build190"
-    `java-library`
-    `maven-publish`
+    id("com.vanniktech.maven.publish") version "0.15.1"
 }
 
 repositories {
@@ -31,18 +33,24 @@ tasks {
     }
 }
 
-artifacts {
-    archives(tasks.jar)
-}
+val mavenPublish = extensions["mavenPublish"] as MavenPublishPluginExtension
+mavenPublish.sonatypeHost = SonatypeHost.S01
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "com.github.Syer10"
-            artifactId = project.name
-            version = "0.24.2-desktop"
-
-            from(components["java"])
+// Read in the signing.properties file if it is exists
+val signingPropsFile = rootProject.file("release/signing.properties")
+if (signingPropsFile.exists()) {
+    Properties().apply {
+        signingPropsFile.inputStream().use {
+            load(it)
+        }
+    }.forEach { key1, value1 ->
+        val key = key1.toString()
+        val value = value1.toString()
+        if (key == "signing.secretKeyRingFile") {
+            // If this is the key ring, treat it as a relative path
+            project.ext.set(key, rootProject.file(value).absolutePath)
+        } else {
+            project.ext.set(key, value)
         }
     }
 }
